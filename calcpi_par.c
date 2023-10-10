ea38aa7f931b8a2a075b457ecf4e87f0c016142a
DanielNevesPP <85492821+DanielNevesPP@users.noreply.github.com>
2023-10-10 14:38:44 -0300

    Add files via upload

---
+++ calcpi_par.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "mpi.h"
+
+#define N 1000000000
+
+int main(int argc, char *argv[]) { /* calcpi_par.c  */
+    double pi, mypi = 0.0f; 
+    long i;
+    int meu_ranque, num_procs;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &meu_ranque); 
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs); 
+
+    for (i = meu_ranque*N/num_procs; i < (meu_ranque+1)*N/num_procs; i++) {
+         double t = (double) ((i+0.5)/N);
+         mypi += 4.0/(1.0+t*t);
+    }
+    MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if(meu_ranque == 0){
+        printf("pi = %f\n", pi/N);
+    }
+
+    MPI_Finalize();
+    return(0);
+}
